81b270c39740b43a51ca32231403f874a1ee4d5c
DXPGH <dxpbusiness@gmail.com>
2022-10-28 09:42:09 -0500

    implemented initial student class

--- miniproject01/Program.cs
+++ miniproject01/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    class Student
+    {
+        public int id;
+        public string name;
+        public string dob;
+        public char grade;
+        private string[]? courses;
+
+        public Student(int id, string name, string dob, char grade)
+        {
+            this.id = id;
+            this.name = name;
+            this.dob = dob;
+            this.grade = grade;
+        }
+
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Student test = new Student(01, "Jack", "04/12/1956", 'B');
+        Console.WriteLine("My ID is {0}, My name is {1}, and my DOB is {2} with a grade of {3}", test.id, test.name, test.dob, test.grade);
+
+        Console.ReadLine();
+    }
+}
